4a280bb8a47d5f2ae70f0038621638eca588adef
Domir77 <101285232+Domir77@users.noreply.github.com>
2022-05-23 23:39:14 +0100

    Распредилили все функции  по модулям

--- main.py
+++ main.py
@@ -1,66 +1,35 @@
-# импорты
-from configparser import ConfigParser
+#Основной файл программы, исполняемый модуль, модуль верхнего уровня
+#Здесь не используем объявление функций
 
 
-# глобальные переменные
-FIELD = [['']*3 for _ in range(3)]
-PLAYERS = {}
-SAVES = {}
-
-#PLAYERS = {'Ivan':[1,1,0]}
-#SAVES = {('ivan','ai1'):[[]],
-#        ('ivan','oleg'):[[]]}
 
 
-# функции
-def field():
-    global FIELD
-    pass
+from players import *
+from help import show_help, show_message
 
-def show_help():
-    pass
-
-def read():
-    global PLAYERS, SAVES
-    config = ConfigParser()
-
-    if config.read('data.ini', encoding='utf-8'):
-
-        PLAYERS = {name.title: [int(n) for n in score.split(',')]
-                   for name, score in config['Scores'].items()}
-
-        SAVES = {tuple(name.split(';')):
-                     [[' ' if c == '-' else c for c in field[i:i+3]]
-                       for i in range(0,9,3)]
-                 for name , field in config['Saves'].items()}
-
-        return True if config['General']['first'] == 'yes' else False
-    else:
-        raise FileExistsError
-
-def save():
-    config = ConfigParser()
-    config['Scores'] = {name: '.'.join(str(n) for n in score)
-                      for name, score in PLAYERS.items()}
-    config['Saves'] = {';'.join(name): ''.join(['-' if c == ' ' else c for r in field for c in r])
-                     for name, field in SAVES.items()}
-    config['General']['first'] = 'no'
-
-    with open('data.ini', 'w', encoding='utf-8') as config_file:
-        config.write(config_file)
-
+#Приветствие
+show_message('КРЕСТИКИ-НОЛИКИ')
 
 # чтение .ini файла
-if read():
-    show_help()
+    # '-i' - запустить в интерактивном меню в edit configurations
+#if read_ini():
+#    show_help()
 
 
 # запуск суперцикла
 while True:
-    command = input()
+    command = input('_>')
 
     if command in ('quit', 'выход'):
         # обработка завершения работы приложения
         break
+    elif command in ('new', 'yes', 'новая', 'да'):
+        #есть ли текущий игрок
+        if not PLAYER:
+            #запрос имени игрока
+            player_name()
+
+
+
 
     # ввод имени игрока
